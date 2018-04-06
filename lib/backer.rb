f531ca2f2679d431fab008c94f387147f544b7e2
Veronica Bustabad <vbustabad@gmail.com>
2018-04-06 15:11:09 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,14 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
   end
 
-  def back_project(project)
+  def back_project(magic)
     @backed_projects = []
-    @backed_projects << project
+    @backed_projects << magic
   end
+
+
 end
